e144275d9a27923f13968f387a0b62efbd9cc3a6
wafflehops <kevinliuyo@gmail.com>
2022-11-02 01:37:12 -0500

    changed slide-combine-slide to one line

--- src/move_board.py
+++ src/move_board.py
@@ -5,9 +5,7 @@ def move_board_right(board):
     moved_board = board
     
     for i in range(len(board)):
-        moved_board[i] = slide_right(moved_board[i])
-        moved_board[i] = combine_right(moved_board[i])
-        moved_board[i] = slide_right(moved_board[i])
+        moved_board[i] = slide_right(combine_right(slide_right(moved_board[i])))
     
     return moved_board
 
@@ -16,9 +14,7 @@ def move_board_left(board):
     moved_board = board
     
     for i in range(len(board)):
-        moved_board[i] = slide_left(moved_board[i])
-        moved_board[i] = combine_left(moved_board[i])
-        moved_board[i] = slide_left(moved_board[i])
+        moved_board[i] = slide_left(combine_left(slide_left(moved_board[i])))
     
     return moved_board
 
@@ -29,9 +25,7 @@ def move_board_up(board):
     for i in range(len(board)):
         col = [[board[x][i]] for x in range(0, len(board))]
             
-        col = slide_up(col)
-        col = combine_up(col)
-        col = slide_up(col)
+        col = slide_up(combine_up(slide_up(col)))
         
         for j in range(len(board)):
             moved_board[j][i] = input[j][0]
@@ -45,9 +39,7 @@ def move_board_down(board):
     for i in range(len(board)):
         col = [[board[x][i]] for x in range(0, len(board))]
         
-        col = slide_down(col)
-        col = combine_down(col)
-        col = slide_down(col)
+        col = slide_down(combine_down(slide_down(col)))
         
         for j in range(len(board)):
             moved_board[j][i] = input[j][0]
