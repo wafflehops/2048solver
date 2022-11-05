bc1a6f50ba0be223e1164059c5ae9b118fbcb266
wafflehops <kevinliuyo@gmail.com>
2022-11-04 23:52:50 -0500

    solved but its instant

--- app.py
+++ app.py
@@ -2,19 +2,16 @@ import tkinter as tk
 import random
 import colors as c
 from src.find_path import *
-
-
+from time import sleep
 class Game(tk.Frame):
     def __init__(self):
         tk.Frame.__init__(self)
         self.grid()
-        self.master.title('2048')
-
+        self.master.title('2048 solver')
         self.main_grid = tk.Frame(
             self, bg=c.GRID_COLOR, bd=3, width=400, height=400)
         self.main_grid.grid(pady=(80, 0))
         self.make_GUI()
-        
         self.mainloop()
 
 
@@ -33,25 +30,49 @@ class Game(tk.Frame):
                 cell_frame.pack_propagate(False)
                 cell_frame.grid(row=i, column=j, padx=5, pady=5)
                 cell_number = tk.Label(cell_frame, text="").place(relx=.5, rely=.5, anchor="center")
-                # cell_number.grid(row=i, column=j)
-                # cell_number.bind("<Button-1>", self.click_number)
                 cell_data = {"frame": cell_frame, "number": cell_number}
                 cell_frame.bind("<Button-1>", self.click_frame)
                 row.append(cell_data)
             self.cells.append(row)
-    
+        
+        start_button = tk.Button(self, text="start", command=self.solve).place(relx=0.5, y=40, anchor="center")
+        
+    def solve(self):
+        shortest_path = find_path(self.matrix)
+        print(shortest_path)
+        self.play(shortest_path)
+        
+    def play(self, path):
+        for move in path:
+            if move == '1':
+                self.up()
+            if move == '2':
+                self.right()
+            if move == '3':
+                self.down()
+            if move == '4':
+                self.left()
+
+
+    def update_matrix(self):
+        for i in range(4):
+            for j in range(4):
+                label = self.cells[i][j]["frame"].winfo_children()[0]
+                self.matrix[i][j] = 0 if label.cget('text') == "" else int(label.cget('text'))
+
     def click_frame(self, event):
         label = event.widget.winfo_children()[0]
+        frame = event.widget
         label.configure(
             text=self.next_number(label.cget('text'))
         )
         number = 0 if label.cget('text') == "" else int(label.cget('text'))
         if number == 0:
-                    event.widget.configure(bg=c.EMPTY_CELL_COLOR)
+                    frame.configure(bg=c.EMPTY_CELL_COLOR)
                     label.configure(
                         bg=c.EMPTY_CELL_COLOR, text="")
         else:
-            event.widget.configure(
+            frame.configure(
                 bg=c.CELL_COLORS[number])
             label.configure(
                 bg=c.CELL_COLORS[number],
@@ -59,10 +80,8 @@ class Game(tk.Frame):
                 font=c.CELL_NUMBER_FONTS[number],
                 text=str(number))
 
-        # event.widget.configure(bg=self.next_color(event.widget.cget('bg')))
-
+        self.update_matrix()
         
-
     def next_number(self, current_number):
         if current_number == "":
             return "2"
@@ -78,12 +97,12 @@ class Game(tk.Frame):
                 cell_value = self.matrix[i][j]
                 if cell_value == 0:
                     self.cells[i][j]["frame"].configure(bg=c.EMPTY_CELL_COLOR)
-                    self.cells[i][j]["number"].configure(
+                    self.cells[i][j]["frame"].winfo_children()[0].configure(
                         bg=c.EMPTY_CELL_COLOR, text="")
                 else:
                     self.cells[i][j]["frame"].configure(
                         bg=c.CELL_COLORS[cell_value])
-                    self.cells[i][j]["number"].configure(
+                    self.cells[i][j]["frame"].winfo_children()[0].configure(
                         bg=c.CELL_COLORS[cell_value],
                         fg=c.CELL_NUMBER_COLORS[cell_value],
                         font=c.CELL_NUMBER_FONTS[cell_value],
@@ -91,25 +110,25 @@ class Game(tk.Frame):
         self.update_idletasks()
 
 
-    def left(self, event):
+    def left(self):
         self.matrix = move_board_left(self.matrix)
         self.update_GUI()
    
 
 
-    def right(self, event):
+    def right(self):
         self.matrix = move_board_right(self.matrix)
         self.update_GUI()
         
 
 
-    def up(self, event):
+    def up(self):
         self.matrix = move_board_up(self.matrix)
         self.update_GUI()
        
 
 
-    def down(self, event):
+    def down(self):
         self.matrix = move_board_down(self.matrix)
         self.update_GUI()
     
--- src/slide.py
+++ src/slide.py
@@ -1,23 +1,23 @@
 def slide_right(row):
-    filtered = list(filter(lambda x : (x is not 0), row))
+    filtered = list(filter(lambda x : (x != 0), row))
     
     return [0] * (len(row) - len(filtered)) + filtered
     
     
 def slide_left(row):
-    filtered = list(filter(lambda x : (x is not 0), row))
+    filtered = list(filter(lambda x : (x != 0), row))
     
     return filtered + [0] * (len(row) - len(filtered)) 
 
 
 def slide_up(col):
-    filtered = list(list(filter(lambda x : (x[0] is not 0), col)))
+    filtered = list(list(filter(lambda x : (x[0] != 0), col)))
     
     return filtered + [[0]] * (len(col) - len(filtered)) 
 
 
 def slide_down(col):
-    filtered = list(list(filter(lambda x : (x[0] is not 0), col)))
+    filtered = list(list(filter(lambda x : (x[0] != 0), col)))
     
     return [[0]] * (len(col) - len(filtered)) + filtered
     
